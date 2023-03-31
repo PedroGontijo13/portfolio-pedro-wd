22bdfbf2ab650a36e412c48a9c5d17bd56cb53ed
Pedro Gontijo <70620630+PedroGontijo13@users.noreply.github.com>
2023-03-31 14:15:20 -0700

    ✅

---
+++ src/pages/Portfolio/Portfolio.jsx
@@ -0,0 +1,39 @@
+import React, { useContext } from "react";
+import styled from "styled-components";
+import ThemeContext from "../../context/ThemeContext";
+import ProjectHeading from "../../components/ProjectHeading/ProjectHeading";
+import ProjectCard from "../../components/Card/ProjectCard/ProjectCard";
+import ProjectImg from "../../assets/project.jpg";
+const PortfolioContainer = styled.div`
+  padding: 2rem 0;
+  background-color: ${(props) => (props.theme === "light" ? "#fff" : "#333")};
+  color: ${(props) => (props.theme === "light" ? "#333" : "#fff")};
+  .inner {
+    width: 1140px;
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    gap: 4rem;
+  }
+`;
+
+const Portfolio = () => {
+  const { theme } = useContext(ThemeContext);
+  return (
+    <PortfolioContainer theme={theme}>
+      <div className="inner">
+        <ProjectHeading />
+        <ProjectCard
+          type={"Web Development"}
+          title={"React Movies"}
+          content={
+            "Movies page that contains movie information, such as ratings. Using React Js and Axios to fetch the Movie Db Api data."
+          }
+          image={ProjectImg}
+        />
+      </div>
+    </PortfolioContainer>
+  );
+};
+
+export default Portfolio;
