e6ac0dbcab18e9b80c251349806a93abe958548d
Pedro Gontijo <70620630+PedroGontijo13@users.noreply.github.com>
2023-04-01 22:09:08 -0700

    MentorShip✅

--- src/components/Card/MentorShipCard/MentorShipCard.jsx
+++ src/components/Card/MentorShipCard/MentorShipCard.jsx
@@ -2,15 +2,15 @@ import React, { useContext } from "react";
 import styled from "styled-components";
 import ThemeContext from "../../../context/ThemeContext";
 import icon from "../../../assets/emoji.png";
-
 const MentorShipCardWrapper = styled.div`
   position: relative;
-  width: 262px;
+  width: 100%;
   height: auto;
 
   background: ${(props) => (props.theme === "light" ? "#333" : "#fff")};
   color: ${(props) => (props.theme === "light" ? "#fff" : "#333")};
   border-radius: 12px;
+  border: 2px solid ${(props) => (props.theme === "light" ? "#fff" : "#333")};
 
   display: flex;
   align-items: center;
@@ -21,28 +21,38 @@ const MentorShipCardWrapper = styled.div`
   .flex {
     display: flex;
   }
+
   .flex img {
-    width: 31.45px;
-    height: 36px;
+    width: 12%;
+    height: 12%;
+    position: absolute;
+    left: -16px;
   }
+
   .flex h6 {
     margin: auto;
   }
+
   h6 {
-    font-style: normal;
+    font-size: 28px;
     font-weight: 600;
-    font-size: 16px;
-    display: flex;
-    align-items: center;
+    line-height: 24px;
     letter-spacing: 1px;
-    margin-top: 56px;
-    margin-bottom: 12px;
+    text-align: left;
+  }
+
+  .row-area {
+    margin: 20px;
+  }
+
+  .margin10 {
+    margin: 10px 0;
   }
 
   small {
     font-style: normal;
     font-weight: 400;
-    font-size: 14px;
+    font-size: 18px;
     line-height: 26px;
     display: flex;
     align-items: center;
@@ -51,46 +61,34 @@ const MentorShipCardWrapper = styled.div`
   }
 `;
 
-const EllipseShape = styled.div`
-  width: 76px;
-  height: 76px;
-  background: linear-gradient(134.01deg, #4ca9ff 38.91%, #3bf686 103.79%);
-  border-radius: 50%;
-  #inner {
-    width: 72px;
-    height: 72px;
-    background: ${(props) => (props.theme === "light" ? "#333" : "#fff")};
-    border-radius: 50%;
-
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    svg {
-      font-size: 32px;
-    }
-  }
-`;
-
 const MentorShipCard = () => {
   const { theme } = useContext(ThemeContext);
   return (
     <MentorShipCardWrapper theme={theme}>
-      <div>
-        <div className="flex">
-          <img src={icon} />
-          <h6>dawkjioadjwi</h6>
+      <div className="row-area">
+        <div className="flex margin10">
+          <img className="center" src={icon} />
+          <h6>Minggu Pertama</h6>
         </div>
-        <small>hadhwuhhbdaw</small>
+        <small>
+          Membahas dasar dari website seperti tag atribut dan element.{" "}
+        </small>
+      </div>
+      <div className="row-area">
         <div className="flex">
-          <img src={icon} />
-          <h6>dawkjioadjwi</h6>
+          <img className="center" src={icon} />
+          <h6>Minggu Kedua</h6>
         </div>
-        <small>hadhwuhhbdaw</small>
+        <small>Membahas seputar Pengembangan website - SDLC dasar.</small>
+      </div>
+      <div className="row-area">
         <div className="flex">
-          <img src={icon} />
-          <h6>dawkjioadjwi</h6>
+          <img className="center" src={icon} />
+          <h6>Minggu Ketiga</h6>
         </div>
-        <small>hadhwuhhbdaw</small>
+        <small>
+          Membahas seputar konsep layouting seperti flexbox dan grid.
+        </small>
       </div>
     </MentorShipCardWrapper>
   );
